754977be67602b50b1daabab2b0db8c06b46bf57
hm5650 <hmmm5651@gmail.com>
2026-07-06 23:44:34 +0500

    H
    E

--- SA2_Function_C.lua
+++ SA2_Function_C.lua
@@ -9,8 +9,6 @@ local UserInputService = game:GetService("UserInputService")
 local GetMouseLocation = UserInputService.GetMouseLocation
 
 local functions = {}
-local currentTool = nil
-local characterAddedConnection = nil
 
 functions.GetScreenPosition = function(Vector)
     local Vec3, OnScreen = WorldToScreen(Camera, Vector)
@@ -33,45 +31,15 @@ functions.GetMousePosition = function()
     return GetMouseLocation(UserInputService)
 end
 
-local function GetGunInternal()
+functions.GetGun = function(Plr)
     local Character = lplr.Character
-    if not Character then return nil end
-    
+    if not Character then return end
     for _,v in ipairs(Character:GetChildren()) do
         if functions.IsTool(v) then
             return v
         end
     end
-    return nil
 end
-
-functions.GetGun = function(Plr)
-    if currentTool and (not currentTool.Parent or currentTool.Parent ~= lplr.Character) then
-        currentTool = nil
-    end
-    if not currentTool then
-        currentTool = GetGunInternal()
-    end
-    
-    return currentTool
-end
-
-functions.ReapplyTool = function()
-    currentTool = nil
-    return functions.GetGun()
-end
-
-local function OnCharacterAdded(character)
-    currentTool = nil
-    task.wait(0.5)
-    functions.ReapplyTool()
-end
-
-if lplr.Character then
-    OnCharacterAdded(lplr.Character)
-end
-
-characterAddedConnection = lplr.CharacterAdded:Connect(OnCharacterAdded)
 
 functions.HitChance = function(Percentage)
     Percentage = math.floor(Percentage)
@@ -82,13 +50,6 @@ end
 
 functions.Direction = function(Origin, Pos)
     return (Pos - Origin).Unit * 1000
-end
-
-functions.Cleanup = function()
-    if characterAddedConnection then
-        characterAddedConnection:Disconnect()
-        characterAddedConnection = nil
-    end
 end
 
 return functions
