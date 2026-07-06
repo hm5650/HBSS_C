6ead4b67e5d6dccb900bd9bdad09bf2ec0bf6a6b
hm5650 <hmmm5651@gmail.com>
2026-07-06 22:12:19 +0500

    Jd

---
+++ SA2_FindTool_C.lua
@@ -0,0 +1,5 @@
+local FindTool = {
+    CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS_C/refs/heads/main/SA2_CTween_C.lua"))(),
+    PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS_C/refs/heads/main/SA2_PathFind_C.lua"))()
+}
+return FindTool
