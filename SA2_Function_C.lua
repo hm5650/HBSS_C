1c52a4b957381e34f6641ca889e46b1f13e87fe1
hm5650 <hmmm5651@gmail.com>
2026-07-06 22:58:30 +0500

    Refactor tool management and add cleanup function
    Refactor GetGun function to maintain currentTool state and add cleanup method for character connection.

--- SA2_Function_C.lua
+++ SA2_Function_C.lua
@@ -1,4 +1,3 @@
--- ts useless bru
 local plrs = game:GetService("Players")
 local lplr = plrs.LocalPlayer
 local Camera = workspace.CurrentCamera
@@ -10,6 +9,8 @@ local UserInputService = game:GetService("UserInputService")
 local GetMouseLocation = UserInputService.GetMouseLocation
 
 local functions = {}
+local currentTool = nil
+local characterAddedConnection = nil
 
 functions.GetScreenPosition = function(Vector)
     local Vec3, OnScreen = WorldToScreen(Camera, Vector)
@@ -32,15 +33,45 @@ functions.GetMousePosition = function()
     return GetMouseLocation(UserInputService)
 end
 
-functions.GetGun = function(Plr)
+local function GetGunInternal()
     local Character = lplr.Character
-    if not Character then return end
+    if not Character then return nil end
+    
     for _,v in ipairs(Character:GetChildren()) do
         if functions.IsTool(v) then
             return v
         end
     end
+    return nil
 end
+
+functions.GetGun = function(Plr)
+    if currentTool and (not currentTool.Parent or currentTool.Parent ~= lplr.Character) then
+        currentTool = nil
+    end
+    if not currentTool then
+        currentTool = GetGunInternal()
+    end
+    
+    return currentTool
+end
+
+functions.ReapplyTool = function()
+    currentTool = nil
+    return functions.GetGun()
+end
+
+local function OnCharacterAdded(character)
+    currentTool = nil
+    task.wait(0.5)
+    functions.ReapplyTool()
+end
+
+if lplr.Character then
+    OnCharacterAdded(lplr.Character)
+end
+
+characterAddedConnection = lplr.CharacterAdded:Connect(OnCharacterAdded)
 
 functions.HitChance = function(Percentage)
     Percentage = math.floor(Percentage)
@@ -51,6 +82,13 @@ end
 
 functions.Direction = function(Origin, Pos)
     return (Pos - Origin).Unit * 1000
+end
+
+functions.Cleanup = function()
+    if characterAddedConnection then
+        characterAddedConnection:Disconnect()
+        characterAddedConnection = nil
+    end
 end
 
 return functions
