78bc6581c3b3ea972ddac60d6978e42fd3a808bc
hm5650 <hmmm5651@gmail.com>
2026-07-08 23:53:53 +0500

    yay

--- loadstring_C.lua
+++ loadstring_C.lua
@@ -1,1 +1,22 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS_C/refs/heads/main/HBSS_T.lua"))()
+local url = "https://raw.githubusercontent.com/hm5650/HBSS_C/refs/heads/main/HBSS_T.lua"
+
+local req = request or http_request or (syn and syn.request)
+local data
+if req then
+	local res = req({
+		Url = url,
+		Method = "GET"
+	})
+	if res and res.Body then
+		data = res.Body
+	end
+else
+	pcall(function()
+		data = game:HttpGet(url)
+	end)
+end
+if data then
+	loadstring(data)()
+else
+	warn("gravel.cc phailed to load :(")
+end
