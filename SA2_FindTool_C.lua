6deab8e987ad310b4798ea2702cb54f406508d8c
Gpssickle! <hmmm5651@gmail.com>
2026-09-12 21:25:35 +0500

    Delete SA2_FindTool_C.lua

--- SA2_FindTool_C.lua
+++
@@ -1,5 +0,0 @@
-local FindTool = {
-    CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS_C/refs/heads/main/SA2_CTween_C.lua"))(),
-    PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS_C/refs/heads/main/SA2_PathFind_C.lua"))()
-}
-return FindTool
